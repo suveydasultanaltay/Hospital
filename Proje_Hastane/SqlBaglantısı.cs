5914d36e73e12310063c9729baf0a1e72d7f453e
Süveyda Sultan Altay <101633467+suveydasultanaltay@users.noreply.github.com>
2022-05-25 23:42:57 +0300

    değişiklikler
    formların devamı

--- Proje_Hastane/SqlBaglantısı.cs
+++ Proje_Hastane/SqlBaglantısı.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SqlClient;
 namespace Proje_Hastane
 {
     internal class SqlBaglantısı
     {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection ()
+        }
     }
 }
